ea2e4355a04d7c287996fbb7016d5aabf9e49ba1
Ashleigh Maund <ashleigh.maund@bcsg.com>
2016-11-22 17:32:30 +0000

    creating an empty scoreboard, with name of each player, 10 score columns and column for overall score

--- src/modules/scoreboard.js
+++ src/modules/scoreboard.js
@@ -1,5 +1,6 @@
 var scoreboard = function (playerCount) {
     var playerCount = playerCount;
+    var columnCount = 10;
 
     this.initScoreboard = function () {
         document.getElementById('scoreboard').classList.remove('hide-element');
@@ -10,10 +11,30 @@ var scoreboard = function (playerCount) {
     };
     
     this.createRow = function (i) {
-        var row = document.createElement('tr');
-        row.id = 'player'+i+'Row';
-        document.getElementById('scoreboardBody').appendChild(row);
-        document.getElementById('player'+i+'Row').innerHTML = "<td>" + window['player'+i].name + "</td><td>" + window['player'+i].score + "</td>";
+        var idOfNewRow = 'player'+i+'Row';
+        this.createElement('tr', idOfNewRow, '', 'scoreboardBody', '');
+        this.createElement('td', 'nameColumn'+i, '', idOfNewRow, window['player'+i].name);
+        for (var j=1; j<=columnCount; j++) {
+            this.createElement('td', '', 'column'+j, idOfNewRow, '');
+        }
+        this.createElement('td', 'scoreColumn'+i, '', idOfNewRow, window['player'+i].score);
+
+        // var scoreColumn = document.createElement('td');
+        // scoreColumn.id = 'scoreColumn'+i;
+        // document.getElementById(idOfNewRow).appendChild(scoreColumn);
+        // document.getElementById('scoreColumn'+i).innerHTML = window['player'+i].score;
+
+
+    };
+    
+    this.createElement = function (elementType, elementId, elementClassList, idOflementToAppendChildTo, valueToPlaceInNewElement) {
+        var element = document.createElement(elementType);
+        element.id = elementId;
+        element.classList = elementClassList;
+        document.getElementById(idOflementToAppendChildTo).appendChild(element);
+        if (elementId.length > 0) {
+            document.getElementById(elementId).innerHTML = valueToPlaceInNewElement;
+        }
     };
 
 
