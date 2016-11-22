b906f6dc4eaf8666f528928547b07e0d894805e0
Ashleigh Maund <ashleigh.maund@bcsg.com>
2016-11-22 15:44:57 +0000

    switching to the scoreboard view once game has started and players have been named

---
+++ src/modules/scoreboard.js
@@ -0,0 +1,5 @@
+var scoreboard = function () {
+    document.getElementById('scoreboard').classList.remove('hide-element');
+    document.getElementById('initialisingGame').classList.add('hide-element');
+
+};
